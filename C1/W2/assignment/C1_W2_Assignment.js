1199b37b7f8cae809b5888dc09efe88d7e43ede8
Balázs Suhajda <suhajdab@gmail.com>
2021-07-15 16:52:55 +0200

    clean code, ditch alerts, solve assignment

--- C1/W2/assignment/C1_W2_Assignment.js
+++ C1/W2/assignment/C1_W2_Assignment.js
@@ -1,137 +1,182 @@
-import {FMnistData} from './fashion-data.js';
+import { FMnistData } from "./fashion-data.js";
 var canvas, ctx, saveButton, clearButton;
-var pos = {x:0, y:0};
+var pos = { x: 0, y: 0 };
 var rawImage;
 var model;
 
-function getModel() {
-    
-    // In the space below create a convolutional neural network that can classify the 
-    // images of articles of clothing in the Fashion MNIST dataset. Your convolutional
-    // neural network should only use the following layers: conv2d, maxPooling2d,
-    // flatten, and dense. Since the Fashion MNIST has 10 classes, your output layer
-    // should have 10 units and a softmax activation function. You are free to use as
-    // many layers, filters, and neurons as you like.  
-    // HINT: Take a look at the MNIST example.
-    model = tf.sequential();
-    
-    // YOUR CODE HERE
-    
-    
-    // Compile the model using the categoricalCrossentropy loss,
-    // the tf.train.adam() optimizer, and accuracy for your metrics.
-    model.compile(// YOUR CODE HERE);
-    
-    return model;
+const getModel = () => {
+  // In the space below create a convolutional neural network that can classify the
+  // images of articles of clothing in the Fashion MNIST dataset. Your convolutional
+  // neural network should only use the following layers: conv2d, maxPooling2d,
+  // flatten, and dense. Since the Fashion MNIST has 10 classes, your output layer
+  // should have 10 units and a softmax activation function. You are free to use as
+  // many layers, filters, and neurons as you like.
+  // HINT: Take a look at the MNIST example.
+  model = tf.sequential();
+
+  model.add(
+    tf.layers.conv2d({
+      inputShape: [28, 28, 1],
+      kernelSize: 3,
+      filters: 32,
+      activation: "relu",
+    })
+  );
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+  model.add(tf.layers.dropout({ rate: 0.25 }));
+  model.add(
+    tf.layers.conv2d({ kernelSize: 3, filters: 64, activation: "relu" })
+  );
+  model.add(tf.layers.maxPooling2d({ poolSize: [2, 2] }));
+  model.add(tf.layers.flatten());
+  model.add(tf.layers.dense({ units: 128, activation: "relu" }));
+  model.add(tf.layers.dropout({ rate: 0.5 }));
+  model.add(tf.layers.dense({ units: 64, activation: "relu" }));
+  model.add(tf.layers.dropout({ rate: 0.5 }));
+  model.add(tf.layers.dense({ units: 10, activation: "softmax" }));
+
+  // Compile the model using the categoricalCrossentropy loss,
+  // the tf.train.adam() optimizer, and accuracy for your metrics.
+  model.compile({
+    optimizer: tf.train.adam(),
+    loss: "categoricalCrossentropy",
+    metrics: ["accuracy"],
+  });
+
+  return model;
+};
+
+const train = async (model, data) => {
+  // Set the following metrics for the callback: 'loss', 'val_loss', 'accuracy', 'val_accuracy'.
+  const metrics = ["loss", "val_loss", "acc", "val_acc"];
+
+  // Create the container for the callback. Set the name to 'Model Training' and
+  // use a height of 1000px for the styles.
+  const container = { name: "Model Training", styles: { height: 1000 } };
+
+  // Use tfvis.show.fitCallbacks() to setup the callbacks.
+  // Use the container and metrics defined above as the parameters.
+  const fitCallbacks = tfvis.show.fitCallbacks(container, metrics);
+
+  const BATCH_SIZE = 512;
+  const TRAIN_DATA_SIZE = 6000;
+  const TEST_DATA_SIZE = 1000;
+
+  // Get the training batches and resize them. Remember to put your code
+  // inside a tf.tidy() clause to clean up all the intermediate tensors.
+  // HINT: Take a look at the MNIST example.
+  const [trainXs, trainYs] = tf.tidy(() => {
+    const d = data.nextTrainBatch(TRAIN_DATA_SIZE);
+    const dataSet = d.xs.reshape([TRAIN_DATA_SIZE, 28, 28, 1]);
+    const labelSet = d.labels;
+    return [dataSet, labelSet];
+  });
+
+  // Get the testing batches and resize them. Remember to put your code
+  // inside a tf.tidy() clause to clean up all the intermediate tensors.
+  // HINT: Take a look at the MNIST example.
+  const [testXs, testYs] = tf.tidy(() => {
+    const d = data.nextTrainBatch(TEST_DATA_SIZE);
+    const dataSet = d.xs.reshape([TEST_DATA_SIZE, 28, 28, 1]);
+    const labelSet = d.labels;
+    return [dataSet, labelSet];
+  });
+
+  return model.fit(trainXs, trainYs, {
+    batchSize: BATCH_SIZE,
+    validationData: [testXs, testYs],
+    epochs: 100,
+    shuffle: true,
+    callbacks: fitCallbacks,
+  });
+};
+
+function setPosition(e) {
+  pos.x = e.clientX - 100;
+  pos.y = e.clientY - 100;
 }
 
-async function train(model, data) {
-        
-    // Set the following metrics for the callback: 'loss', 'val_loss', 'accuracy', 'val_accuracy'.
-    const metrics = // YOUR CODE HERE    
-
-        
-    // Create the container for the callback. Set the name to 'Model Training' and 
-    // use a height of 1000px for the styles. 
-    const container = // YOUR CODE HERE   
-    
-    
-    // Use tfvis.show.fitCallbacks() to setup the callbacks. 
-    // Use the container and metrics defined above as the parameters.
-    const fitCallbacks = // YOUR CODE HERE
-    
-    const BATCH_SIZE = 512;
-    const TRAIN_DATA_SIZE = 6000;
-    const TEST_DATA_SIZE = 1000;
-    
-    // Get the training batches and resize them. Remember to put your code
-    // inside a tf.tidy() clause to clean up all the intermediate tensors.
-    // HINT: Take a look at the MNIST example.
-    const [trainXs, trainYs] = // YOUR CODE HERE
-
-    
-    // Get the testing batches and resize them. Remember to put your code
-    // inside a tf.tidy() clause to clean up all the intermediate tensors.
-    // HINT: Take a look at the MNIST example.
-    const [testXs, testYs] = // YOUR CODE HERE
-
-    
-    return model.fit(trainXs, trainYs, {
-        batchSize: BATCH_SIZE,
-        validationData: [testXs, testYs],
-        epochs: 10,
-        shuffle: true,
-        callbacks: fitCallbacks
-    });
-}
-
-function setPosition(e){
-    pos.x = e.clientX-100;
-    pos.y = e.clientY-100;
-}
-    
 function draw(e) {
-    if(e.buttons!=1) return;
-    ctx.beginPath();
-    ctx.lineWidth = 24;
-    ctx.lineCap = 'round';
-    ctx.strokeStyle = 'white';
-    ctx.moveTo(pos.x, pos.y);
-    setPosition(e);
-    ctx.lineTo(pos.x, pos.y);
-    ctx.stroke();
-    rawImage.src = canvas.toDataURL('image/png');
-}
-    
-function erase() {
-    ctx.fillStyle = "black";
-    ctx.fillRect(0,0,280,280);
-}
-    
-function save() {
-    var raw = tf.browser.fromPixels(rawImage,1);
-    var resized = tf.image.resizeBilinear(raw, [28,28]);
-    var tensor = resized.expandDims(0);
-    
-    var prediction = model.predict(tensor);
-    var pIndex = tf.argMax(prediction, 1).dataSync();
-    
-    var classNames = ["T-shirt/top", "Trouser", "Pullover", 
-                      "Dress", "Coat", "Sandal", "Shirt",
-                      "Sneaker",  "Bag", "Ankle boot"];
-            
-            
-    alert(classNames[pIndex]);
-}
-    
-function init() {
-    canvas = document.getElementById('canvas');
-    rawImage = document.getElementById('canvasimg');
-    ctx = canvas.getContext("2d");
-    ctx.fillStyle = "black";
-    ctx.fillRect(0,0,280,280);
-    canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("mousedown", setPosition);
-    canvas.addEventListener("mouseenter", setPosition);
-    saveButton = document.getElementById('sb');
-    saveButton.addEventListener("click", save);
-    clearButton = document.getElementById('cb');
-    clearButton.addEventListener("click", erase);
+  if (e.buttons != 1) return;
+  ctx.beginPath();
+  ctx.lineWidth = 24;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = "white";
+  ctx.moveTo(pos.x, pos.y);
+  setPosition(e);
+  ctx.lineTo(pos.x, pos.y);
+  ctx.stroke();
+  rawImage.src = canvas.toDataURL("image/png");
 }
 
+const erase = () => {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 280, 280);
+};
 
-async function run() {
-    const data = new FMnistData();
-    await data.load();
-    const model = getModel();
-    tfvis.show.modelSummary({name: 'Model Architecture'}, model);
-    await train(model, data);
-    await model.save('downloads://my_model');
-    init();
-    alert("Training is done, try classifying your drawings!");
-}
+const timeout = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
 
-document.addEventListener('DOMContentLoaded', run);
+const save = () => {
+  var raw = tf.browser.fromPixels(rawImage, 1);
+  var resized = tf.image.resizeBilinear(raw, [28, 28]);
+  var tensor = resized.expandDims(0);
 
+  var prediction = model.predict(tensor);
+  var pIndex = tf.argMax(prediction, 1).dataSync();
 
+  var classNames = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+  ];
 
+  console.log(prediction.dataSync());
+  console.log(
+    `%c This looks like a(n) ${classNames[pIndex]}!`,
+    "color: lime; font-size: 12px"
+  );
+};
+
+const initDrawing = () => {
+  canvas = document.getElementById("canvas");
+  rawImage = document.getElementById("canvasimg");
+  ctx = canvas.getContext("2d");
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, 280, 280);
+  canvas.addEventListener("mousemove", draw);
+  canvas.addEventListener("mousedown", setPosition);
+  canvas.addEventListener("mouseenter", setPosition);
+  saveButton = document.getElementById("sb");
+  saveButton.addEventListener("click", save);
+  clearButton = document.getElementById("cb");
+  clearButton.addEventListener("click", erase);
+};
+
+const run = async () => {
+  const data = new FMnistData();
+  await data.load();
+  const model = getModel();
+  tfvis.show.modelSummary({ name: "Model Architecture" }, model);
+  await timeout(10);
+  if (!confirm("Start training model?")) return;
+  await train(model, data);
+  if (confirm("Save model?")) {
+    await model.save("downloads://my_model");
+  }
+  initDrawing();
+  console.log(
+    "%c 👏 Training is done, try classifying your handwriting!",
+    "color: #f60; font-size: 14px"
+  );
+};
+
+document.addEventListener("DOMContentLoaded", run);
