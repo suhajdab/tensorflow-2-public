3577c1756c2ef45cce01a73f2af676debc544163
Balázs Suhajda <suhajdab@gmail.com>
2021-07-15 18:19:41 +0200

    cleanup code

--- C1/W3/ungraded_labs/mobilenet.js
+++ C1/W3/ungraded_labs/mobilenet.js
@@ -1,11 +1,11 @@
-const img = document.getElementById("img");
-const outp = document.getElementById("output");
-mobilenet.load().then((model) => {
-  model.classify(img).then((predictions) => {
-    console.log(predictions);
-    for (var i = 0; i < predictions.length; i++) {
-      outp.innerHTML +=
-        "<br/>" + predictions[i].className + " : " + predictions[i].probability;
-    }
+(async () => {
+  const img = document.getElementById("img");
+  const outp = document.getElementById("output");
+  const model = await mobilenet.load();
+  const predictions = await model.classify(img);
+
+  console.log(JSON.stringify(predictions, null, "\t"));
+  predictions.forEach((pred) => {
+    outp.innerHTML += "<br/>" + pred.className + " : " + pred.probability;
   });
-});
+})();
