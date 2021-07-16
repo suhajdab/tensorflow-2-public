313b435c996f068ec94be0065ee7bc185298518c
Balázs Suhajda <suhajdab@gmail.com>
2021-07-16 11:22:56 +0200

    Use files generated by colab
    Running code in jupyter locally failed, but colab.research.google.com came to the rescue

--- C1/W3/ungraded_labs/linear.js
+++ C1/W3/ungraded_labs/linear.js
@@ -1,5 +1,5 @@
 (async () => {
-  const MODEL_URL = "output_files/model.json"; // point to github version, as jupyter notebook failed to run
+  const MODEL_URL = "./model.json";
   const model = await tf.loadLayersModel(MODEL_URL);
   model.summary();
   const input = tf.tensor2d([10.0], [1, 1]);
