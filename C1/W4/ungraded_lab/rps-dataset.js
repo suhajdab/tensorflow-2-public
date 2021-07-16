e827a1e485aadce33373e13baa5f004ee06280c5
Balázs Suhajda <suhajdab@gmail.com>
2021-07-16 16:23:29 +0200

    cleanup

--- C1/W4/ungraded_lab/rps-dataset.js
+++ C1/W4/ungraded_lab/rps-dataset.js
@@ -1,6 +1,7 @@
 class RPSDataset {
   constructor() {
-    this.labels = []
+    this.labels = [];
+    this.xs = null;
   }
 
   addExample(example, label) {
@@ -14,17 +15,19 @@ class RPSDataset {
       oldX.dispose();
     }
   }
-  
+
   encodeLabels(numClasses) {
     for (var i = 0; i < this.labels.length; i++) {
       if (this.ys == null) {
-        this.ys = tf.keep(tf.tidy(
-            () => {return tf.oneHot(
-                tf.tensor1d([this.labels[i]]).toInt(), numClasses)}));
+        this.ys = tf.keep(
+          tf.tidy(() =>
+            tf.oneHot(tf.tensor1d([this.labels[i]]).toInt(), numClasses)
+          )
+        );
       } else {
-        const y = tf.tidy(
-            () => {return tf.oneHot(
-                tf.tensor1d([this.labels[i]]).toInt(), numClasses)});
+        const y = tf.tidy(() =>
+          tf.oneHot(tf.tensor1d([this.labels[i]]).toInt(), numClasses)
+        );
         const oldY = this.ys;
         this.ys = tf.keep(oldY.concat(y, 0));
         oldY.dispose();
